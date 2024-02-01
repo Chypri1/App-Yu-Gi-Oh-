1ea4e597ff1805ed274b465a374d5d0b64fe6cd2
Cyprien Pennachi <cyprienpennachi@gmail.com>
2024-02-01 19:20:44 +0100

    deuxieme page créé
    TODO. : trouver une solution avec menu navig et une liste de bouton et commencer à essayer de faire les requetes

--- app/src/main/java/com/example/appyugioh/MainActivity.java
+++ app/src/main/java/com/example/appyugioh/MainActivity.java
@@ -4,24 +4,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
-    Button boutonRechercheCarte;
+    protected Button boutonRechercheCarte;
+
+    protected Button boutonRechercheDeck;
+
+    protected ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainactivity);
 
-        this.boutonRechercheCarte = findViewById(R.id.boutonRecherche);
+        this.boutonRechercheCarte = findViewById(R.id.boutonRechercheCarte);
+
+        this.boutonRechercheDeck= findViewById(R.id.boutonRechercheDeck);
+
+        this.imageView = findViewById(R.id.imageView);
         boutonRechercheCarte.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent recherche = new Intent(getApplicationContext(),RechercheCarte.class);
-                startActivity(recherche);
+                Intent rechercheCarte = new Intent(getApplicationContext(),RechercheCarte.class);
+                startActivity(rechercheCarte);
                 finish();
             }
         });
+
+
+        boutonRechercheDeck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent rechercheDeck = new Intent(getApplicationContext(),RechercheDeck.class);
+                startActivity(rechercheDeck);
+                finish();
+            }
+        });
+
+
     }
 }
