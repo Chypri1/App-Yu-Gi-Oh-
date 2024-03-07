7e007d68fd30139f0d70466565fc7dc1f0fee88f
Cyprien Pennachi <cyprienpennachi@gmail.com>
2024-03-07 17:05:16 +0100

    enregistrementCarte
    dans galerie

--- app/src/main/java/com/example/appyugioh/interfaceGraphique/EnregistrerCarte.java
+++ app/src/main/java/com/example/appyugioh/interfaceGraphique/EnregistrerCarte.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.hardware.Camera;
 import android.hardware.camera2.*;
 
@@ -24,6 +26,13 @@ import android.widget.LinearLayout;
 
 import com.example.appyugioh.R;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class EnregistrerCarte extends Activity {
 
@@ -37,6 +46,8 @@ public class EnregistrerCarte extends Activity {
     protected ImageView imageCam;
 
     protected Button boutonAccesCamera;
+
+    protected Button boutonEnregistrementCarte;
 
     private String photoPath = null;
 
@@ -56,10 +67,45 @@ public class EnregistrerCarte extends Activity {
 
         boutonAccesCamera = findViewById(R.id.boutonAccesCamera);
 
+        boutonEnregistrementCarte = findViewById(R.id.boutonEnregistrementCarte);
+
         boutonAccesCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 prendreUnePhoto();
+                //imageCam.setVisibility(View.VISIBLE);
+            }
+        });
+
+        boutonEnregistrementCarte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(nomCarte.getText()!= null || nomCarte.getText().equals(R.string.nom_carte)){
+
+                }
+                if(nomEdition.getText()!= null || nomEdition.getText().equals(R.string.edition)) {
+
+                }
+
+                // Vérifier si l'imageView n'est pas vide
+                if (imageCam.getDrawable() != null) {
+                    // Récupérer le Drawable de l'ImageView
+                    Drawable drawable = imageCam.getDrawable();
+                    // Vérifier si le Drawable est une instance de BitmapDrawable
+                    if (drawable instanceof BitmapDrawable) {
+                        // Convertir le Drawable en Bitmap
+                        Bitmap image = ((BitmapDrawable) drawable).getBitmap();
+                        // Enregistrer l'image dans le stockage de l'appareil
+                        String imageSavedPath = saveImageToGallery(image, nomCarte.getText().toString());
+                        // Vérifier si l'enregistrement de l'image a réussi
+                        if (imageSavedPath != null) {
+                            // L'image a été enregistrée avec succès
+                            // Vous pouvez effectuer d'autres opérations ici si nécessaire
+                        } else {
+                            // Gérer le cas où l'enregistrement de l'image a échoué
+                        }
+                    }
+                }
             }
         });
     }
@@ -68,8 +114,6 @@ public class EnregistrerCarte extends Activity {
     private void prendreUnePhoto()  {
 
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-            if (intent.resolveActivity(getPackageManager()) != null) {
                 // Récupérer l'ID de la caméra frontale
                 String frontCameraId = null ;
                 CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
@@ -79,31 +123,75 @@ public class EnregistrerCarte extends Activity {
                         if (characteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT) {
                             // Caméra frontale trouvée
                             frontCameraId = cameraId;
+                            String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                            File photoDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                            File photoFile = File.createTempFile("photo" +time, ".jpg",photoDir);
+                            //enregistrer le chemin
+                            this.photoPath = photoFile.getAbsolutePath();
                             break;
                         }
                     }
                 } catch (CameraAccessException e) {
                     e.printStackTrace();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
 
-                if (frontCameraId != null) {
-                    // La caméra frontale a été trouvée, vous pouvez maintenant l'utiliser
+        if (frontCameraId != null) {
+                    startActivityForResult(intent, RETOUR_PRENDRE_PHOTO);
                 } else {
-                    // Aucune caméra frontale disponible
+                    // aller dans les parametres de l'appareil pour l'activer
                 }
             }
-        }
+
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode,resultCode,data);
-        if(requestCode == RETOUR_PRENDRE_PHOTO && resultCode == RESULT_OK)
-        {
-            Bitmap image = BitmapFactory.decodeFile(photoPath);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RETOUR_PRENDRE_PHOTO) {
+            if (resultCode == RESULT_OK ) {
+                // La photo a été prise et le résultat est valide
+                Bundle extras = data.getExtras();
+                if (extras != null) {
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    // Utiliser l'image capturée
+                    imageCam.setImageBitmap(imageBitmap);
 
-            imageCam.setImageBitmap(image);
+                }
+            }
+        }
+    }
+
+    private String saveImageToGallery(Bitmap image, String imageName) {
+        // Vérifier si le stockage externe est disponible en écriture
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            // Définir le répertoire de destination pour enregistrer l'image
+            File directory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "VotreDossier");
+            // Créer le répertoire s'il n'existe pas
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+            // Enregistrer l'image dans le répertoire
+            File imageFile = new File(directory, imageName + ".jpg");
+            try {
+                FileOutputStream outputStream = new FileOutputStream(imageFile);
+                image.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                outputStream.close();
+                // Ajouter l'image à la galerie
+                MediaStore.Images.Media.insertImage(getContentResolver(), imageFile.getAbsolutePath(), imageName, "Description de l'image");
+                // Retourner le chemin de l'image enregistrée
+                return imageFile.getAbsolutePath();
+            } catch (IOException e) {
+                e.printStackTrace();
+                // Gérer l'exception si l'enregistrement de l'image échoue
+                return null;
+            }
+        } else {
+            // Gérer le cas où le stockage externe n'est pas disponible
+            return null;
         }
     }
 }
+
+
